455cac5e2c852a36257d52a414e4e35cf968cc35
viktorstebner <92970272+viktorstebner@users.noreply.github.com>
2022-07-30 19:38:46 +0200

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggle_mobile_nav() {
+    document.getElementById("mobile-nav-toggle").classList.toggle("flip");
+    document.getElementById("nav").classList.toggle("mobile-show-nav");
+}
