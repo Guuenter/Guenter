04d84ef2a023ca13de72c9f19183aa6feb2dd9d7
viktorstebner <92970272+viktorstebner@users.noreply.github.com>
2022-08-06 17:09:46 +0200

    Version E

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function toggle_mobile_nav() {
-    document.getElementById("mobile-nav-toggle").classList.toggle("flip");
-    document.getElementById("nav").classList.toggle("mobile-show-nav");
+//function toggle_mobile_nav() {
+//    document.getElementById("mobile-nav-toggle").classList.toggle("flip");
+//    document.getElementById("nav").classList.toggle("mobile-show-nav");
+//}
+
+function toggleDrawer(drawer_id) {
+    document.getElementById(drawer_id).classList.toggle("drawer-closed");
 }
